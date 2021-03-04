df248434619c5ec9be40454420561479f909b871
Geonsang Yoo <geonsangyoo@gmail.com>
2021-03-04 18:45:22 +0900

    allowed host added

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '09+u1@bi39lq7ht&#_#77hr@)andxa5ary_=92!%ig0879kxyb'
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = [
-    'ec2-18-182-34-191.ap-northeast-1.compute.amazonaws.com',
+    '35.72.0.124',
     '127.0.0.1',
 ]
 
